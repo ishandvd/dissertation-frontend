94b3b08e1a721827c3f4bf81ff6a90924c8251b7
Ishan <id360@cam.ac.uk>
2023-03-02 00:09:52 +0000

    switched to using HTTP requests instead of sockets

--- src/components/AnalysisPage.js
+++ src/components/AnalysisPage.js
@@ -3,67 +3,27 @@ import colourScheme from './colourScheme';
 import RecorderJSDemo from './RecorderJSDemo';
 import SheetDisplay from './SheetDisplay';
 import sampleDrumHits from './sampleDrumHits';
-import { io } from "socket.io-client";
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const AnalysisPage = () => {
 
-    const [socketInstance, setSocketInstance] = useState("");
     const [loading, setLoading] = useState(true);
-  
-    const connectToServer = () => {
-        const socket = io("localhost:5000/", {
-        transports: ["websocket"],
-        cors: {
-            origin: "http://localhost:3000/",
-        },
-        });
-    
-        setSocketInstance(socket);
-    
-        socket.on("connect", (data) => {
-        console.log("You have connected. Server : ", data);
-        });
-    
-        setLoading(false);
 
-        socket.on("disconnect", (data) => {
-            console.log("You have disconnected. Server: ", data);
-        });
-
-        socket.on("backingtrack", (data) => {
-            console.log("backing track received", data);
-        });
-    
-    }
-
-    useEffect(() => {
-        if(socketInstance === "") {
-            console.log("Analysis Page: connecting to server");
-            connectToServer();
-        }
-    }, [socketInstance]);
 
     const sendBackingTrack = (event) => {
         const file = event.target.files[0];
-        console.log("Analysis Page: backing track selected", file.name);
-        const reader = new FileReader();
-        reader.onload = () => {
-            const buffer = reader.result;
-            socketInstance.emit("backingtrack", buffer);
-            console.log("Analysis Page: backing track sent")
-        };
-        reader.readAsArrayBuffer(file);
-    }
-
-    const handleFileUpload = (event) => {
-        if(socketInstance === "") {
-            connectToServer()
-            sendBackingTrack(event);
-        } else {
-            sendBackingTrack(event);
+        console.log("Analysis Page: sending backing track");
+        let data = new FormData();
+        data.append('backing_track', file, "backing.wav");
+        const config = {
+            headers: {'content-type': 'multipart/form-data'}
         }
-    };
+        // Send post request and receive response, then log it
+        axios.post('http://localhost:5000/backing-track', data, config).then((response) => {
+            console.log(response);
+        })
+    }
 
   return (
     <div className="analysis">
